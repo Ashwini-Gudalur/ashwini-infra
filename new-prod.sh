b5932a0a8b62f88c2e0a2cb5ba63eb591933464a
Vivek Singh <petmongrels@gmail.com>
2021-06-22 16:00:17 +0530

    open postgres

--- new-prod.sh
+++ new-prod.sh
@@ -1,7 +1,7 @@
 #### 192.168.1.241
 
 #open this server to public
-sh /root/source/ashwini-infra/scripts/open-ssh-to-public.sh
+sh /root/source/ashwini-infra/scripts/open-ssh.sh
 
 #connect remotely to this server
 ssh -p 2223 root@localhost
---
+++ scripts/open-postgres.sh
@@ -0,0 +1,1 @@
+ssh -R2225:localhost:5432 root@143.110.188.91 -i /root/.ssh/abi
