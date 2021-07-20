be18af4903de064e8dffc70d881be1aaae379e9e
Vivek Singh <petmongrels@gmail.com>
2021-07-20 15:56:23 +0530

    run openmrs in debug

---
+++ openmrs/openmrs-remote-debug.sh
@@ -0,0 +1,8 @@
+export OPENMRS_SERVER_USER=bahmni
+export OPENMRS_SERVER_RUN=/opt/openmrs/bin/start.sh
+export OPENMRS_SERVER_DEBUG=/opt/openmrs/bin/debug.sh
+export CUR_USER=`/usr/bin/whoami`
+export OPENMRS_LIB_CACHE=/tmp/*.openmrs-lib-cache
+
+. /etc/openmrs/openmrs.conf
+runuser -u $OPENMRS_SERVER_USER nohup $JAVA_HOME/bin/java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 /opt/openmrs/lib/openmrs.jar >> /var/log/openmrs/openmrs.log 2>&1 &
