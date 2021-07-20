7ab03e456417b3a10e84db2b0e1328c59655b423
Vivek Singh <petmongrels@gmail.com>
2021-07-20 17:38:20 +0530

    create dirs

--- openmrs/openmrs-remote-debug.sh
+++ openmrs/openmrs-remote-debug.sh
@@ -5,5 +5,20 @@ export CUR_USER=`/usr/bin/whoami`
 export OPENMRS_LIB_CACHE=/tmp/*.openmrs-lib-cache
 export JAVA_HOME=/usr/java/jdk1.8.0_291-amd64
 
+create_dirs() {
+    if [[ ! -e /var/run/openmrs ]]; then
+        ln -s /opt/openmrs/run /var/run/openmrs
+    fi
+
+    if [[ ! -e /var/run/openmrs/openmrs ]]; then
+        ln -s /opt/openmrs/openmrs /var/run/openmrs/openmrs
+    fi
+
+    if [[ ! -e /var/log/openmrs ]]; then
+        ln -s /opt/openmrs/log /var/log/openmrs
+    fi
+}
+
 . /etc/openmrs/openmrs.conf
-runuser -u $OPENMRS_SERVER_USER nohup $JAVA_HOME/bin/java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 /opt/openmrs/lib/openmrs.jar >> /var/log/openmrs/openmrs.log 2>&1 &
+create_dirs
+runuser -u $OPENMRS_SERVER_USER -- nohup $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar /opt/openmrs/lib/openmrs.jar >> /var/log/openmrs/openmrs.log 2>&1 &
