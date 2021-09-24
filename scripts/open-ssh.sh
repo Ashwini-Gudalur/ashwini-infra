647c2ece1e3afe9dc43f56620119c81fc84a4e21
Vivek Singh <petmongrels@gmail.com>
2021-09-24 17:09:00 +0530

    keep alive managed by cloud server

--- scripts/open-ssh.sh
+++ scripts/open-ssh.sh
@@ -1,2 +1,2 @@
 echo "opening reverse ssh port on 2300"
-ssh -o ServerAliveInterval=10 -R2300:localhost:22 sam@139.59.23.103 -i /root/.ssh/id_rsa_temp
+ssh -o -R2300:localhost:22 sam@139.59.23.103 -i /root/.ssh/id_rsa_temp
